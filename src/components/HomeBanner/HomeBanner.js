0d6e8a5bf61d517eee63921c7add7e01790c3bd4
raj <rraju12@gmail.com>
2021-03-07 09:05:52 +0000

    Reposition image 1

--- src/components/HomeBanner/HomeBanner.js
+++ src/components/HomeBanner/HomeBanner.js
@@ -11,7 +11,7 @@ const useStyles = makeStyles(theme => ({
     )`,
     backgroundRepeat: 'no-repeat',
     backgroundAttachment: 'fixed',
-    backgroundSize: 'cover',
+    backgroundSize: 'auto',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
